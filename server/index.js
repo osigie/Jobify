d3e815b70877a3d05439b839560f2240f9c1633b
usser.email <kenosagie88@gmail.com>
2022-05-07 23:42:00 +0100

    added gitignore and dotenv

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { notFoundMiddleware } from "./middlewares/NotFound.js";
+import dotenv from "dotenv"
+dotenv.config()
+import { notFoundMiddleware } from "./middlewares/notFound.js";
+import { errorHandlerMiddleware } from "./middlewares/errorHandler.js";
 const app = express();
 
 app.get("/", (req, res) => {
@@ -7,6 +10,7 @@ app.get("/", (req, res) => {
 });
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
