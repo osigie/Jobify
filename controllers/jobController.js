64fdeda1bf907bf9edfe58de477c5f917f4da2cb
osigie <90554061+osigie@users.noreply.github.com>
2022-05-14 17:41:06 +0100

    done with search and filter in backend

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,6 @@
 import Job from "../models/Job.js";
 import { StatusCodes } from "http-status-codes";
-import moment from "moment"
+import moment from "moment";
 import {
   NotFoundError,
   BadRequestError,
@@ -31,8 +31,42 @@ export const deleteJobs = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "Job successfully deleted" });
 };
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
+  const { status, jobType, sort, search } = req.query;
 
+  const queryObj = {
+    createdBy: req.user.userId,
+  };
+
+  //conditions
+
+  if (status != "all") {
+    queryObj.status = status;
+  }
+
+  if (jobType != "all") {
+    queryObj.jobType = jobType;
+  }
+
+  if (search) {
+    queryObj.position = { $regex: search, $options: "i" };
+  }
+  let result = Job.find(queryObj);
+
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt')
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt')
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position')
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position')
+  }
+
+  const jobs = await result;
   res
     .status(StatusCodes.OK)
     .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
@@ -59,46 +93,45 @@ export const updateJobs = async (req, res) => {
 };
 
 export const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$status", count: { $sum: 1 } } },
+  ]);
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
 
-    let stats = await Job.aggregate([
-        { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
-        { $group: { _id: '$status', count: { $sum: 1 } } },
-      ])
-      stats = stats.reduce((acc, curr) => {
-        const { _id: title, count } = curr
-        acc[title] = count
-        return acc
-      }, {})
-      const defaultStats = {
-        pending: stats.pending || 0,
-        interview: stats.interview || 0,
-        declined: stats.declined || 0,
-      }
-    
-      let monthlyApplications = await Job.aggregate([
-        { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
-        {
-          $group: {
-            _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
-            count: { $sum: 1 },
-          },
-        },
-        { $sort: { '_id.year': -1, '_id.month': -1 } },
-        { $limit: 6 },
-      ])
-      monthlyApplications = monthlyApplications
-        .map((item) => {
-          const {
-            _id: { year, month },
-            count,
-          } = item
-          const date = moment()
-            .month(month - 1)
-            .year(year)
-            .format('MMM Y')
-          return { date, count }
-        })
-        .reverse()
-    
-      res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
